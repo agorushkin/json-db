7be859a2d7b7971439b64b361d8fdc907daf3ca5
Arsenii Gorushkin <arsenii.gorushkin@gmail.com>
2023-09-16 14:05:03 +1200

    info: initial commit

---
+++ mod.ts
@@ -0,0 +1,29 @@
+export class JSONDB<T = unknown> {
+  data: T;
+  path: string;
+
+  constructor(path: string, base?: T) {
+    this.data = base as T;
+    this.path = path;
+  }
+
+  read = async () => {
+    const file = await Deno.readTextFile(this.path);
+    const json = JSON.parse(file);
+
+    this.data = json as T;
+  };
+
+  write = async () => {
+    const data = JSON.stringify(this.data);
+    const status = await Deno.writeTextFile(`${ this.path }.tmp`, data)
+      .then(() => true)
+      .catch(() => false);
+
+    return status;
+  };
+
+  commit = () => {
+    Deno.renameSync(`${ this.path }.tmp`, this.path);
+  };
+}
